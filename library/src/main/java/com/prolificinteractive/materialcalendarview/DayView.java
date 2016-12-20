e69b0bf9549d31b3336ef210349a1316b5b0e400
Bodo1981 <christian.bahl@gmail.com>
2016-12-20 18:22:54 +0100

    Update DayView.java

--- library/src/main/java/com/prolificinteractive/materialcalendarview/DayView.java
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/DayView.java
@@ -194,11 +194,11 @@ class DayView extends CheckedTextView {
     private static Drawable generateBackground(int color, int fadeTime, Rect bounds) {
         StateListDrawable drawable = new StateListDrawable();
         drawable.setExitFadeDuration(fadeTime);
-        drawable.addState(new int[]{android.R.attr.state_checked}, generateCircleDrawable(color));
+        
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            drawable.addState(new int[]{android.R.attr.state_pressed}, generateRippleDrawable(color, bounds));
+            drawable.addState(new int[]{android.R.attr.state_pressed, android.R.attr.state_checked}, generateRippleDrawable(color, bounds));
         } else {
-            drawable.addState(new int[]{android.R.attr.state_pressed}, generateCircleDrawable(color));
+            drawable.addState(new int[]{android.R.attr.state_pressed, android.R.attr.state_checked}, generateCircleDrawable(color));
         }
 
         drawable.addState(new int[]{}, generateCircleDrawable(Color.TRANSPARENT));
